879e2224e3ff0a2240da17066655827105b52d11
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-10-28 23:36:21 -0400

    CreacionFormularioLogin

--- SistemaGym.UI.Windows/Login.Designer.cs
+++ SistemaGym.UI.Windows/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SistemaGym.UI.Windows
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Text = "Form1";
         }
 
         #endregion
