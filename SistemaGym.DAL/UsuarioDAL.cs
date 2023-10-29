624097dd1c93640faec870034cb0aa915bdffb81
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-10-28 23:22:00 -0400

    UsuarioDAL ConsultaLogin
    consultar datos para iniciar sesion

--- SistemaGym.DAL/UsuarioDAL.cs
+++ SistemaGym.DAL/UsuarioDAL.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SistemaGym.DAL
 {
-    public class UsuarioDAL
+    public class UsuarioDAL : ConexionDAL
     {
+        public int ConsultaLogin(string usuario, string contrasena)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+
+
+            int count;
+            Conexion.Open();
+            string Query = "Select Count(*) From Usuario where usuario = '" + usuario + "'" +
+                "and contraseña = '" + contrasena + "'";
+            SqlCommand cmd = new SqlCommand(Query, Conexion);
+            count = Convert.ToInt32(cmd.ExecuteScalar());
+            Conexion.Close();
+
+            return count;
+
+        }
     }
 }
