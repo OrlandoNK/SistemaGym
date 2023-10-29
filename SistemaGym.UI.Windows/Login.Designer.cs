f7c56786c3f2ef1b801344d98f51e4fb25669993
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-10-29 00:14:59 -0400

    formulariologin2

--- SistemaGym.UI.Windows/Login.Designer.cs
+++ SistemaGym.UI.Windows/Login.Designer.cs
@@ -3,12 +3,12 @@
     partial class Login
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,17 +23,81 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btnIniciarSesion = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            txtContrasena = new TextBox();
+            SuspendLayout();
+            // 
+            // btnIniciarSesion
+            // 
+            btnIniciarSesion.Location = new Point(315, 249);
+            btnIniciarSesion.Name = "btnIniciarSesion";
+            btnIniciarSesion.Size = new Size(122, 23);
+            btnIniciarSesion.TabIndex = 0;
+            btnIniciarSesion.Text = "Iniciar Sesion";
+            btnIniciarSesion.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(218, 117);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(218, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(306, 109);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(181, 23);
+            txtUsuario.TabIndex = 3;
+            // 
+            // txtContrasena
+            // 
+            txtContrasena.Location = new Point(306, 157);
+            txtContrasena.Name = "txtContrasena";
+            txtContrasena.Size = new Size(181, 23);
+            txtContrasena.TabIndex = 4;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(txtContrasena);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnIniciarSesion);
+            Name = "Login";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnIniciarSesion;
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private TextBox txtContrasena;
     }
 }
