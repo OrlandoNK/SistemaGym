5f799dcb0b80684afd59c406078a6699c86ee9b5
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-10-28 23:46:05 -0400

    capabll

--- SistemaGym.BLL/UsuarioBLL.cs
+++ SistemaGym.BLL/UsuarioBLL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaGym.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SistemaGym.BLL
 {
     public class UsuarioBLL
     {
+         readonly UsuarioDAL usuarioDAL = new UsuarioDAL();
+        public int ConultaLogin(string usuario, string contrasena)
+        {
+            return usuarioDAL.ConsultaLogin(usuario, contrasena);
+        }
     }
 }
