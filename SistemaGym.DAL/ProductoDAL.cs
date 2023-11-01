3dfbe5eec99b87958bc79b8b9fe791231ccd8ac5
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-01 00:21:02 -0400

    Metodo para Insertar Productos (ProductoDAL)

--- SistemaGym.DAL/ProductoDAL.cs
+++ SistemaGym.DAL/ProductoDAL.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using SistemaGym.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SistemaGym.DAL
 {
-    public class ProductoDAL
+    public class ProductoDAL : ConexionDAL
     {
+        /* Metodo para Insertar Producto */
+        public static void InsertarProductos(ProductoEntity producto)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = new SqlConnection();
+
+            Conexion.Open();
+            string Insertar = "INSERT INTO(Categorias, No_Existencias, Nombre, PrecioUnitario) VALUES(@Categorias, @No_Existencias, @Nombre, @PrecioUnitario)";
+            SqlCommand cmd = new SqlCommand(Insertar, Conexion);
+
+            cmd.Parameters.AddWithValue("@Categorias", producto.Categorias);
+            cmd.Parameters.AddWithValue("@No_Existencias", producto.No_Existencias);
+            cmd.Parameters.AddWithValue("@Nombre", producto.Nombre);
+            cmd.Parameters.AddWithValue("@PrecioUnitario", producto.PrecioUnitario);
+
+            cmd.ExecuteNonQuery();
+
+        }
     }
 }
