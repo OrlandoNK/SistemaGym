a4afe358bd088ffbfc673431d08a94b1ba6f49b6
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-11-01 22:57:35 -0400

    Metodo Insertar usuarios

--- SistemaGym.DAL/UsuarioDAL.cs
+++ SistemaGym.DAL/UsuarioDAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -17,13 +18,144 @@ namespace SistemaGym.DAL
 
             int count;
             Conexion.Open();
-            string Query = "SELECT COUNT(*) FROM Usuario WHERE usuario = @Usuario AND contraseña = @Contraseña";
+            string Query = "SELECT COUNT(*) FROM Usuario WHERE usuario = @nombresuario AND contrasena = @contraseña";
             SqlCommand cmd = new SqlCommand(Query, Conexion);
-            cmd.Parameters.AddWithValue("@Usuario", usuario.NombreUsuario);
-            cmd.Parameters.AddWithValue("@Contraseña", usuario.Contrasena);
+            cmd.Parameters.AddWithValue("@nombreusuario", usuario.NombreUsuario);
+            cmd.Parameters.AddWithValue("@contrasena", usuario.Contrasena);
             count = Convert.ToInt32(cmd.ExecuteScalar());
 
             return count;
         }
+        //insertar usuario
+        public static void InsertarUsuario(UsuarioEntity usuario)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+
+            Conexion.Open();
+            string insertar = "Insert into Usuarios(IDRol, Nombre, Apellido, Sexo, Correo, Direccion, FechaRegistro," +
+                " NombreUSuario, Contrasena, Estatus)" +
+                " values(@idrol, @nombre, @apellido, @sexo, @correo, @direccion, @fecharegistro, @nombreusuario, @contrasena, @estatus)";
+            SqlCommand cmd = new SqlCommand(insertar, Conexion);
+            cmd.Parameters.AddWithValue("@idrol", usuario.IDRol);
+            cmd.Parameters.AddWithValue("@nombre", usuario.Nombre);
+            cmd.Parameters.AddWithValue("@apellido", usuario.Apellido);
+            cmd.Parameters.AddWithValue("@sexo", usuario.Sexo);
+            cmd.Parameters.AddWithValue("@correo", usuario.Correo);
+            cmd.Parameters.AddWithValue("@direccion", usuario.Direccion);
+            cmd.Parameters.AddWithValue("@FechaRegistro", usuario.FechaRegistro);
+            cmd.Parameters.AddWithValue("@nombreusuario", usuario.NombreUsuario);
+            cmd.Parameters.AddWithValue("@contrasena", usuario.Contrasena);
+            cmd.Parameters.AddWithValue("@estatus", usuario.Estatus);
+            cmd.ExecuteNonQuery();
+        }
+
+        //metodo actualizar usuario
+        public static void ActualizarUsuario(ClientesEntity clientes)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+
+            Conexion.Open();
+            string actualizar = "Update Clientes set IDUsuario =@idusuario, IDMembresia=@idmembresia, TipoListaCliente=@tipolistacliente " +
+                "TipoCliente =@tipocliente, Nombre=@nombre, Apellido =@apellido, TipoDocumento=@tipodocumento, documento=@documento " +
+                "Direccion =@direccion, Telcell=@telcell, TelRes=@telres, FechaRegistro =@fecharegistro, Estatus =@estatus ";
+            SqlCommand cmd = new SqlCommand(actualizar, Conexion);
+            cmd.Parameters.AddWithValue("@idusuario", clientes.IDUsuario);
+            cmd.Parameters.AddWithValue("@idmembresia", clientes.IDMembresia);
+            cmd.Parameters.AddWithValue("@tipolistacliente", clientes.TipoListaCliente);
+            cmd.Parameters.AddWithValue("@tipocliente", clientes.TipoCliente);
+            cmd.Parameters.AddWithValue("@nombre", clientes.Nombre);
+            cmd.Parameters.AddWithValue("@apellido", clientes.Apellido);
+            cmd.Parameters.AddWithValue("@tipodocumento", clientes.TipoDocumento);
+            cmd.Parameters.AddWithValue("@documento", clientes.Documento);
+            cmd.Parameters.AddWithValue("@direccion", clientes.Direccion);
+            cmd.Parameters.AddWithValue("@telcell", clientes.TelCell);
+            cmd.Parameters.AddWithValue("@telres", clientes.TelRes);
+            cmd.Parameters.AddWithValue("@fecharegistro", clientes.FechaRegistro);
+            cmd.Parameters.AddWithValue("@estatus", clientes.Estatus);
+            cmd.ExecuteNonQuery();
+
+
+        }
+        //funcion eliminar usuario
+
+        public static bool EliminarUsuario(ClientesEntity clientes)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+            bool seElimino;
+
+
+
+            Conexion.Open();
+            string Eliminar = "Delete from Clientes where IDCliente= @idcliente";
+            SqlCommand cmd = new SqlCommand(Eliminar, Conexion);
+            cmd.Parameters.AddWithValue("@idcliente", clientes.IDCliente);
+            seElimino = cmd.ExecuteNonQuery() > 0;
+            return seElimino;
+
+        }
+
+
+        //metodo mostrar usuario
+        public static DataTable MostrarUsuario()
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+            DataTable dt = new DataTable();
+            Conexion.Open();
+            string mostrar = "Select * From Clientes";
+            SqlCommand cmd = new SqlCommand(mostrar, Conexion);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+
+            return dt;
+
+        }
+        //buscar id
+        public static DataTable BuscarID(ClientesEntity clientes)
+        {
+
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+
+            Conexion.Open();
+            DataTable dt = new DataTable();
+            string buscar = "Select * From Clientes where IDCliente= @idcliente";
+            SqlCommand cmd = new SqlCommand(buscar, Conexion);
+            cmd.Parameters.AddWithValue("@idcliente", clientes.IDCliente);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
+        }
+        // buscar por valor
+        public static DataTable ObtenerPorValor(ClientesEntity clientes)
+        {
+
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+
+            Conexion.Open();
+            DataTable dt = new DataTable();
+            string obtenerValor = "Select * from Clientes " +
+                 "Where IDMembresia Like '%' + @idmembresia + '%' or Nombre Like '%' + @nombre + '%' or Apellido Like '%' + @apellido + '%' or " +
+                 "Documento Like '%' + @Documento '%' or Direccion Like '%' + @Direccion + '%' or TelCell Like '%' + @telcell + '%' " +
+                 "TelRes Like '%' + @telres + '%' or Estatus Like '%' + @estatus + '%' ORDER BY Nombre";
+            SqlCommand cmd = new SqlCommand(obtenerValor, Conexion);
+            cmd.Parameters.AddWithValue("@idmembresia", clientes.IDMembresia);
+            cmd.Parameters.AddWithValue("@nombre", clientes.Nombre);
+            cmd.Parameters.AddWithValue("@apellido", clientes.Apellido);
+            cmd.Parameters.AddWithValue("@documento", clientes.Documento);
+            cmd.Parameters.AddWithValue("@direccion", clientes.Direccion);
+            cmd.Parameters.AddWithValue("@telcell", clientes.TelCell);
+            cmd.Parameters.AddWithValue("@telres", clientes.TelRes);
+            cmd.Parameters.AddWithValue("@estatus", clientes.Estatus);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
+        }
     }
 }
+
