24f51043ee54e3de1d05b35229f443fbc471f79a
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-11-02 00:05:37 -0400

    Insertar  y actualizar

--- SistemaGym.Entities/TipoClienteEntity.cs
+++ SistemaGym.Entities/TipoClienteEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace SistemaGym.Entities
 
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
     
     }
 }
