fd284333560f5005d3c88d3215741f0b683044ae
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-02 23:08:59 -0400

    Metodos Actualizar y Eliminar BlacklistCliente capa DAL

--- SistemaGym.DAL/BlacklistClienteDAL.cs
+++ SistemaGym.DAL/BlacklistClienteDAL.cs
@@ -33,5 +33,48 @@ namespace SistemaGym.DAL
 
         }
 
+        /* Metodo Actualizar */
+
+        public static void Actualizar(BlacklistClienteEntity blacklist)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = new SqlConnection();
+
+            Conexion.Open();
+
+            string Actualizar = "UPDATE BlacklistCliente SET IDBlackListCliente = @IDBlackListCliente, IDListaCliente = @IDListaCliente, IDCliente = @IDCliente, Razones = @Razones, FechaInicial = @FechaInicial, FechaVencimiento = @FechaVencimiento, Estatus = @Estatus";
+            SqlCommand cmd = new SqlCommand(Actualizar, Conexion);
+
+            cmd.Parameters.AddWithValue("@IDBlackListCliente", blacklist.IDBlackListCliente);
+            cmd.Parameters.AddWithValue("@IDListaCliente", blacklist.IDListaCliente);
+            cmd.Parameters.AddWithValue("@IDCliente", blacklist.IDCliente);
+            cmd.Parameters.AddWithValue("@Razones", blacklist.Razones);
+            cmd.Parameters.AddWithValue("@FechaInicial", blacklist.FechaInicial);
+            cmd.Parameters.AddWithValue("@FechaVencimiento", blacklist.FechaVencimiento);
+            cmd.Parameters.AddWithValue("@Estatus", blacklist.Estatus);
+
+            cmd.ExecuteNonQuery();
+
+        }
+
+        /* Metodo Eliminar */
+
+        public static bool EliminarByID(BlacklistClienteEntity blacklist)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+            bool seElimino;
+
+            Conexion.Open();
+            string Eliminar = "DELETE FROM BlacklistCliente WHERE IDBlackListCliente = @IDBlackListCliente";
+            SqlCommand cmd = new SqlCommand(Eliminar, Conexion);
+
+            cmd.Parameters.AddWithValue("@IDBlackListCliente", blacklist.IDBlackListCliente);
+            seElimino = cmd.ExecuteNonQuery() > 0;
+
+            return seElimino;
+
+        }
+
     }
 }
