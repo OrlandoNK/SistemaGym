d0a20fe4d0743a68e74acb69182aa41dace65280
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-02 23:30:01 -0400

    Metodos Mostrar, BuscarByID y ObtenerByValor BlacklistCliente capa DAL

--- SistemaGym.DAL/BlacklistClienteDAL.cs
+++ SistemaGym.DAL/BlacklistClienteDAL.cs
@@ -1,6 +1,7 @@
 ﻿using SistemaGym.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -73,6 +74,71 @@ namespace SistemaGym.DAL
             seElimino = cmd.ExecuteNonQuery() > 0;
 
             return seElimino;
+
+        }
+
+        /* Metodo para Mostrar */
+
+        public static DataTable Mostrar()
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+
+            DataTable dataTBL = new DataTable();
+            Conexion.Open();
+            string Mostrar = "SELECT * FROM BlacklistCliente ORDER BY IDCliente";
+            SqlCommand cmd = new SqlCommand(Mostrar, Conexion);
+            SqlDataAdapter adapterDT = new SqlDataAdapter(cmd);
+            adapterDT.Fill(dataTBL);
+
+            return dataTBL;
+        }
+
+        /* Metodo Buscar por ID */
+
+        public static DataTable BuscarByID(BlacklistClienteEntity blacklist)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+            Conexion.Open();
+
+            DataTable dataTBL = new DataTable();
+            string Buscar = "SELECT * FROM BlacklistCliente WHERE IDCliente = @IDCliente";
+            SqlCommand cmd = new SqlCommand(Buscar, Conexion);
+
+            cmd.Parameters.AddWithValue("@IDCliente", blacklist.IDCliente);
+            SqlDataAdapter adapterDT = new SqlDataAdapter(cmd);
+            adapterDT.Fill(dataTBL);
+
+            return dataTBL;
+
+        }
+
+        /* Metodo Obtener por Valor */
+
+        public static DataTable ObtenerByValor(BlacklistClienteEntity blacklist)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = instancia.Conexion();
+
+            Conexion.Open();
+
+            DataTable dataTBL = new DataTable();
+            string GetByValor = "SELECT * FROM BlacklistCliente WHERE IDBlackListCliente LIKE '%' + @IDBlacklistCliente + '%', IDListaCliente LIKE '%' + @IDListaCliente + '%', IDCliente LIKE '%' + @IDCliente + '%', Razones LIKE '%' + @Razones + '%', FechaInicial LIKE '%' + @FechaInicial + '%', FechaVencimiento LIKE '%' + @FechaVencimiento + '%', Estatus LIKE '%' + @Estatus + '%' ORDER BY IDCliente";
+            SqlCommand cmd = new SqlCommand(GetByValor, Conexion);
+
+            cmd.Parameters.AddWithValue("@IDBlackListCliente", blacklist.IDBlackListCliente);
+            cmd.Parameters.AddWithValue("@IDListaCliente", blacklist.IDListaCliente);
+            cmd.Parameters.AddWithValue("@IDCliente", blacklist.IDCliente);
+            cmd.Parameters.AddWithValue("@Razones", blacklist.Razones);
+            cmd.Parameters.AddWithValue("@FechaInicial", blacklist.FechaInicial);
+            cmd.Parameters.AddWithValue("@FechaVencimiento", blacklist.FechaVencimiento);
+            cmd.Parameters.AddWithValue("@Estatus", blacklist.Estatus);
+
+            SqlDataAdapter adapterDT = new SqlDataAdapter(cmd);
+            adapterDT.Fill(dataTBL);
+
+            return dataTBL;
 
         }
 
