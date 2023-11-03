bd908372f89b0716d9341f07cb49cfb2dc4e4c8b
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-02 23:45:41 -0400

    Metodo Insertar de FacturacionMembresia capa DAL

--- SistemaGym.DAL/FacturacionMembresiaDAL.cs
+++ SistemaGym.DAL/FacturacionMembresiaDAL.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using SistemaGym.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SistemaGym.DAL
 {
-    public class FacturacionMembresiaDAL
+    public class FacturacionMembresiaDAL : ConexionDAL
     {
+        /* Metodo Insertar */
+
+        public static void Insertar(FacturaMembresiaEntity facturaMembresia)
+        {
+            ConexionDAL instancia = Instancia();
+            SqlConnection Conexion = new SqlConnection();
+
+            Conexion.Open();
+            string Insertar = "INSERT INTO FacturacionMembresia(IDFactura, IDMembresia, IDCliente, IDUsuario, NCF, FechaEmision, FechaVencimiento, Estatus) " +
+                              " VALUES(@IDFactura, @IDMembresia, @IDCliente, @IDUsuario, @NCF, @FechaEmision, @FechaVencimiento, @Estatus)";
+
+            SqlCommand cmd = new SqlCommand(Insertar, Conexion);
+            cmd.Parameters.AddWithValue("@IDFactura", facturaMembresia.IDFactura);
+            cmd.Parameters.AddWithValue("@IDMembresia", facturaMembresia.IDMembresia);
+            cmd.Parameters.AddWithValue("@IDCliente", facturaMembresia.IDCliente);
+            cmd.Parameters.AddWithValue("@IDUsuario", facturaMembresia.IDUsuario);
+            cmd.Parameters.AddWithValue("@NCF", facturaMembresia.NCF);
+            cmd.Parameters.AddWithValue("@FechaEmision", facturaMembresia.FechaEmision);
+            cmd.Parameters.AddWithValue("@FechaVencimiento", facturaMembresia.FechaVencimiento);
+            cmd.Parameters.AddWithValue("@Estatus", facturaMembresia.Estatus);
+
+            cmd.ExecuteNonQuery();
+
+        }
+
+        /* Metodo Actualizar */
+
+        public static void Actualizar(FacturaMembresiaEntity facturaMembresia)
+        {
+
+        }
+
     }
 }
