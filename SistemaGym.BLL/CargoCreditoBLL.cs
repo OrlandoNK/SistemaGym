e17aa1e4b202d36ec314abd1f389bc80e50b0369
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-07 00:09:30 -0400

    Metodos CRUD CargoCredito capa BLL

--- SistemaGym.BLL/CargoCreditoBLL.cs
+++ SistemaGym.BLL/CargoCreditoBLL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SistemaGym.DAL;
+using SistemaGym.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,39 @@ namespace SistemaGym.BLL
 {
     public class CargoCreditoBLL
     {
+
+        public void Insertar (CargoCreditoEntity cargoCredito)
+        {
+            CargoCreditoDAL.InsertarCargoCredito(cargoCredito);
+        }
+
+        public void Actualizar (CargoCreditoEntity cargoCredito)
+        {
+            CargoCreditoDAL.ActualizarCargoCredito(cargoCredito);
+        }
+
+        public bool Eliminar (CargoCreditoEntity cargoCredito)
+        {
+            return CargoCreditoDAL.EliminarCargoCredito(cargoCredito);
+        }
+
+        public DataTable Mostrar()
+        {
+            DataTable dataTable = CargoCreditoDAL.MostrarCargoCredito();
+            return dataTable;
+        }
+
+        public static DataTable BuscarByID (CargoCreditoEntity cargoCredito)
+        {
+            DataTable dataTable = CargoCreditoDAL.BuscarID(cargoCredito);
+            return dataTable;
+        }
+
+        public static DataTable GetByValor(CargoCreditoEntity cargoCredito)
+        {
+            DataTable dataTable = CargoCreditoDAL.ObtenerPorValor(cargoCredito);
+            return dataTable;
+        }
+
     }
 }
