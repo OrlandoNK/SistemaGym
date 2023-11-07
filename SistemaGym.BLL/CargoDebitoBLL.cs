6abbe2a6d46b6681e08d35ecc0922f8413d632c7
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-07 00:17:06 -0400

    Metodos CRUD CargoDebito en la capa BLL

--- SistemaGym.BLL/CargoDebitoBLL.cs
+++ SistemaGym.BLL/CargoDebitoBLL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SistemaGym.DAL;
+using SistemaGym.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,38 @@ namespace SistemaGym.BLL
 {
     public class CargoDebitoBLL
     {
+        public static void Insertar (CargoDebitoEntity cargoDebito)
+        {
+            CargoDebitoDAL.InsertarCargoDebito(cargoDebito);
+        }
+
+        public static void Actualizar (CargoDebitoEntity cargoDebito)
+        {
+            CargoDebitoDAL.ActualizarCargoDebito(cargoDebito);
+        }
+
+        public static bool Eliminar (CargoDebitoEntity cargoDebito)
+        {
+            return CargoDebitoDAL.EliminarCargoDebito(cargoDebito);
+        }
+
+        public static DataTable Mostrar()
+        {
+            DataTable dataTable = CargoDebitoDAL.MostrarCargoCredito();
+            return dataTable;
+        }
+
+        public static DataTable BuscarByID (CargoDebitoEntity cargoDebito)
+        {
+            DataTable dataTable = CargoDebitoDAL.BuscarID(cargoDebito);
+            return dataTable;
+        }
+
+        public static DataTable ObtenerByValor(CargoDebitoEntity cargoDebito)
+        {
+            DataTable dataTable = CargoDebitoDAL.ObtenerPorValor(cargoDebito);
+            return dataTable;
+        }
+
     }
 }
