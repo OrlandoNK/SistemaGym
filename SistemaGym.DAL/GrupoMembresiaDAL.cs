f51173a94f25a3f4665e6947856286a8159387ad
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-11-06 20:45:15 -0400

    Cambios

--- SistemaGym.DAL/GrupoMembresiaDAL.cs
+++ SistemaGym.DAL/GrupoMembresiaDAL.cs
@@ -12,42 +12,42 @@ namespace SistemaGym.DAL
     public class GrupoMembresia : ConexionDAL
     {
         //metodo insertar GrupoMembresia
-        public static void InsertarCliente(GrupoMembresiaEntity GrupoMembresia)
+        public static void InsertarGrupoMembresia(GrupoMembresiaEntity grupoMembresia)
         {
             ConexionDAL instancia = Instancia();
             SqlConnection Conexion = instancia.Conexion();
 
             Conexion.Open();
-            string insertar = "Insert into GrupoMembresia(IDGrupoMembresia, IDMembresia, FechaRegistro, Estatus)" +
-                " values(@idcliente, @idusuario, @idmembresia, @fecharegistro, @estatus)";
+            string insertar = "Insert into GrupoMembresia(IDMembresia, Nombre, FechaRegistro, Estatus)" +
+                " values(@idmembresia, @nombre, @fecharegistro, @estatus)";
             SqlCommand cmd = new SqlCommand(insertar, Conexion);
-            cmd.Parameters.AddWithValue("@idGrupoMembresia", GrupoMembresia.IDGrupoMembresia);
-            cmd.Parameters.AddWithValue("@idmembresia", GrupoMembresia.IDMembresia);
-            cmd.Parameters.AddWithValue("@fecharegistro", GrupoMembresia.FechaRegistro);
-            cmd.Parameters.AddWithValue("@estatus", GrupoMembresia.Estatus);
+            cmd.Parameters.AddWithValue("@idmembresia", grupoMembresia.IDMembresia);
+            cmd.Parameters.AddWithValue("@nombre", grupoMembresia.Nombre);
+            cmd.Parameters.AddWithValue("@fecharegistro", grupoMembresia.FechaRegistro);
+            cmd.Parameters.AddWithValue("@estatus", grupoMembresia.Estatus);
             cmd.ExecuteNonQuery();
         }
 
         //metodo actualizar GrupoMembresia
-        public static void ActualizarCliente(GrupoMembresiaEntity GrupoMembresia)
+        public static void ActualizarGrupoMembresia(GrupoMembresiaEntity grupoMembresia)
         {
             ConexionDAL instancia = Instancia();
             SqlConnection Conexion = instancia.Conexion();
 
             Conexion.Open();
-            string actualizar = "Update GrupoMembresia set IDGrupoMembresia =@idGrupoMembresia, IDMembresia=@idmembresia, FechaRegistro =@fecharegistro, Estatus =@estatus ";
+            string actualizar = "Update GrupoMembresia set IDMembresia=@idmembresia, Nombre =@nombre, FechaRegistro =@fecharegistro, Estatus =@estatus ";
             SqlCommand cmd = new SqlCommand(actualizar, Conexion);
-            cmd.Parameters.AddWithValue("@idGrupoMembresia", GrupoMembresia.IDGrupoMembresia);
-            cmd.Parameters.AddWithValue("@idmembresia", GrupoMembresia.IDMembresia);
-            cmd.Parameters.AddWithValue("@fecharegistro", GrupoMembresia.FechaRegistro);
-            cmd.Parameters.AddWithValue("@estatus", GrupoMembresia.Estatus);
+            cmd.Parameters.AddWithValue("@idmembresia", grupoMembresia.IDMembresia);
+            cmd.Parameters.AddWithValue("@nombre", grupoMembresia.Nombre);
+            cmd.Parameters.AddWithValue("@fecharegistro", grupoMembresia.FechaRegistro);
+            cmd.Parameters.AddWithValue("@estatus", grupoMembresia.Estatus);
             cmd.ExecuteNonQuery();
 
 
         }
         //funcion eliminar GrupoMembresia
 
-        public static bool EliminarCliente(GrupoMembresiaEntity GrupoMembresia)
+        public static bool EliminarGrupoMembresia(GrupoMembresiaEntity grupoMembresia)
         {
             ConexionDAL instancia = Instancia();
             SqlConnection Conexion = instancia.Conexion();
@@ -56,9 +56,9 @@ namespace SistemaGym.DAL
 
 
             Conexion.Open();
-            string Eliminar = "Delete from GrupoMembresia where IDGrupoMembresia= @idGrupoMembresia";
+            string Eliminar = "Delete from GrupoMembresia where IDGrupoMembresia= @idgrupomembresia";
             SqlCommand cmd = new SqlCommand(Eliminar, Conexion);
-            cmd.Parameters.AddWithValue("@idGrupoMembresia", GrupoMembresia.IDGrupoMembresia);
+            cmd.Parameters.AddWithValue("@idgrupomembresia", grupoMembresia.IDGrupoMembresia);
             seElimino = cmd.ExecuteNonQuery() > 0;
             return seElimino;
 
@@ -82,7 +82,7 @@ namespace SistemaGym.DAL
 
         }
 
-        public static DataTable BuscarID(GrupoMembresiaEntity GrupoMembresia)
+        public static DataTable BuscarID(GrupoMembresiaEntity grupoMembresia)
         {
 
             ConexionDAL instancia = Instancia();
@@ -92,13 +92,13 @@ namespace SistemaGym.DAL
             DataTable dt = new DataTable();
             string buscar = "Select * From GrupoMembresia where IDGrupoMembresia= @idGrupoMembresia";
             SqlCommand cmd = new SqlCommand(buscar, Conexion);
-            cmd.Parameters.AddWithValue("@idGrupoMembresia", GrupoMembresia.IDGrupoMembresia);
+            cmd.Parameters.AddWithValue("@idGrupoMembresia", grupoMembresia.IDGrupoMembresia);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             da.Fill(dt);
             return dt;
         }
         //
-        public static DataTable ObtenerPorValor(GrupoMembresiaEntity GrupoMembresia)
+        public static DataTable ObtenerPorValor(GrupoMembresiaEntity grupoMembresia)
         {
 
             ConexionDAL instancia = Instancia();
@@ -107,13 +107,13 @@ namespace SistemaGym.DAL
             Conexion.Open();
             DataTable dt = new DataTable();
             string obtenerValor = "Select * from GrupoMembresia " +
-                 "Where IDGrupoMembresia Like '%' + @idGrupoMembresia + '%' or IDMembresia Like '%' + @idmembresia + '%' or FechaRegistro Like '%' + @FechaRegistro + '%' or Apellido Like '%' + @apellido + '%' or " +
+                 "Where IDMembresia Like '%' + @idmembresia + '%' or Nombre Like '%' + @nombre + '%' or FechaRegistro Like '%' + @FechaRegistro + '%' or Estatus Like '%' + @estatus + '%' " +
                 " or Estatus Like '%' + @estatus ";
             SqlCommand cmd = new SqlCommand(obtenerValor, Conexion);
-            cmd.Parameters.AddWithValue("@idGrupoMembresia", GrupoMembresia.IDGrupoMembresia);
-            cmd.Parameters.AddWithValue("@idmembresia", GrupoMembresia.IDMembresia);
-            cmd.Parameters.AddWithValue("@FechaRegistro", GrupoMembresia.FechaRegistro);
-            cmd.Parameters.AddWithValue("@estatus", GrupoMembresia.Estatus);
+            cmd.Parameters.AddWithValue("@idmembresia", grupoMembresia.IDMembresia);
+            cmd.Parameters.AddWithValue("@nombre", grupoMembresia.Nombre);
+            cmd.Parameters.AddWithValue("@FechaRegistro", grupoMembresia.FechaRegistro);
+            cmd.Parameters.AddWithValue("@estatus", grupoMembresia.Estatus);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             da.Fill(dt);
             return dt;
