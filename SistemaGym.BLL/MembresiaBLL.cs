c7b81ef49fc0bae3d65bfdd28c29828703a056b5
rosaCarrasco <146145669+rosaCarrasco@users.noreply.github.com>
2023-11-06 23:10:58 -0400

    MetodoCrudMembresiaBLL(hecho)
    Hecho

--- SistemaGym.BLL/MembresiaBLL.cs
+++ SistemaGym.BLL/MembresiaBLL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SistemaGym.DAL;
+using SistemaGym.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,41 @@ namespace SistemaGym.BLL
 {
     public class MembresiaBLL
     {
+        public void Insertar(MembresiaEntity membresia)
+        {
+            MembresiaDAL .InsertarMembresia(membresia);
+        }
+
+    public void Actualizar(MembresiaEntity membresia)
+    {
+        MembresiaDAL.ActualizarMembresia(membresia);
     }
+
+    public bool Eliminar(MembresiaEntity membresia)
+    {
+        return MembresiaDAL.EliminarMembresia(membresia);
+    }
+
+    public DataTable Mostrar()
+    {
+        DataTable dataTable = MembresiaDAL.MostrarMembresia();
+        return dataTable;
+    }
+
+    public DataTable BuscarByID(MembresiaEntity membresia)
+    {
+        DataTable dataTable = MembresiaDAL.BuscarID(membresia);
+        return dataTable;
+    }
+    public DataTable BuscarByValor(MembresiaEntity membresia)
+        {
+            DataTable dataTable = MembresiaDAL.ObtenerPorValor(membresia);
+            return dataTable;
+        }
+    }
+        
+        
+
 }
+
+    
