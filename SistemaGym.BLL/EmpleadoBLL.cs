dbbd2a16a78f93f4bb5c5872b5f5104f0c3c428b
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-06 22:57:50 -0400

    Metodos CRUD Empleado Capa BLL (Listos)

--- SistemaGym.BLL/EmpleadoBLL.cs
+++ SistemaGym.BLL/EmpleadoBLL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SistemaGym.DAL;
+using SistemaGym.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,40 @@ namespace SistemaGym.BLL
 {
     public class EmpleadoBLL
     {
+        private EmpleadoDAL empleadoDAL;
+
+        public void insertarEmpleado (EmpleadoEntity empleado)
+        {
+            EmpleadoDAL.insertarEmpleado(empleado);
+        }
+
+        public void actualizarEmpleado(EmpleadoEntity empleado)
+        {
+            EmpleadoDAL.actualizarEmpleado(empleado);
+        }
+
+        public void eliminarEmpleado(EmpleadoEntity empleado)
+        {
+            empleadoDAL.eliminarEmpleado(empleado);
+        }
+
+        public DataTable mostrarEmpleado()
+        {
+            DataTable dataTBL = EmpleadoDAL.mostrarEmpleado();
+            return dataTBL;
+        }
+
+        public DataTable buscarByID(EmpleadoEntity empleado)
+        {
+            DataTable DTtbl = empleadoDAL.SearchByID(empleado);
+            return DTtbl;
+        }
+
+        public static DataTable GetByValor (EmpleadoEntity empleado)
+        {
+            DataTable dataTable = EmpleadoDAL.GetByValor(empleado);
+            return dataTable;
+        }
+
     }
 }
