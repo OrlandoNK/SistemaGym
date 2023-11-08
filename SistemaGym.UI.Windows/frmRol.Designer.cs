28e7d581b48fbf3077a40f637c6e41ddea2e15c0
KingHernan07 <145953703+KingHernan07@users.noreply.github.com>
2023-11-09 00:23:47 +0100

    ManteniemientoRolHecho
    Todo Listo

---
+++ SistemaGym.UI.Windows/frmRol.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SistemaGym.UI.Windows
+{
+    partial class frmRol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textIDRol = new TextBox();
+            textNombre = new TextBox();
+            textDescripcion = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ID Rol";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Descripcion";
+            // 
+            // textIDRol
+            // 
+            textIDRol.Location = new Point(107, 53);
+            textIDRol.Name = "textIDRol";
+            textIDRol.Size = new Size(100, 23);
+            textIDRol.TabIndex = 3;
+            // 
+            // textNombre
+            // 
+            textNombre.Location = new Point(107, 86);
+            textNombre.Name = "textNombre";
+            textNombre.Size = new Size(100, 23);
+            textNombre.TabIndex = 4;
+            // 
+            // textDescripcion
+            // 
+            textDescripcion.Location = new Point(107, 120);
+            textDescripcion.Name = "textDescripcion";
+            textDescripcion.Size = new Size(100, 23);
+            textDescripcion.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(263, 27);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(240, 150);
+            dataGridView1.TabIndex = 6;
+            // 
+            // frmRol
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(559, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(textDescripcion);
+            Controls.Add(textNombre);
+            Controls.Add(textIDRol);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmRol";
+            Text = "frmRol";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textIDRol;
+        private TextBox textNombre;
+        private TextBox textDescripcion;
+        private DataGridView dataGridView1;
+    }
+}
