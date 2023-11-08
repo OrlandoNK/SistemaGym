904e84febc0cfc2e5647937f7903baa8e9ec1c6f
KingHernan07 <145953703+KingHernan07@users.noreply.github.com>
2023-11-08 23:42:10 +0100

    MantenimientoEmpleadoHecho
    Todo Listo

---
+++ SistemaGym.UI.Windows/frmEmpleado.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace SistemaGym.UI.Windows
+{
+    partial class frmEmpleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            textIDEmpleado = new TextBox();
+            textUsuario = new TextBox();
+            textNombre = new TextBox();
+            textApellido = new TextBox();
+            textTipoDocumento = new TextBox();
+            textDocumento = new TextBox();
+            textDireccion = new TextBox();
+            textTelCell = new TextBox();
+            textTelRes = new TextBox();
+            textFechaRegistro = new TextBox();
+            textEstatus = new TextBox();
+            dgvEmpleado = new DataGridView();
+            IDEmpleado = new DataGridViewTextBoxColumn();
+            Usuario = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Apellido = new DataGridViewTextBoxColumn();
+            TipoDocumento = new DataGridViewTextBoxColumn();
+            Documento = new DataGridViewTextBoxColumn();
+            Direccion = new DataGridViewTextBoxColumn();
+            TellCell = new DataGridViewTextBoxColumn();
+            TelRes = new DataGridViewTextBoxColumn();
+            FechaRegistro = new DataGridViewTextBoxColumn();
+            Estatus = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvEmpleado).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ID Empleado";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(20, 107);
+            label4.Name = "label4";
+            label4.Size = new Size(51, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Apellido";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 136);
+            label5.Name = "label5";
+            label5.Size = new Size(93, 15);
+            label5.TabIndex = 4;
+            label5.Text = "TipoDocumento";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(14, 165);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Documento";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(24, 194);
+            label7.Name = "label7";
+            label7.Size = new Size(57, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Direccion";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(27, 223);
+            label8.Name = "label8";
+            label8.Size = new Size(44, 15);
+            label8.TabIndex = 7;
+            label8.Text = "TellCell";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(27, 253);
+            label9.Name = "label9";
+            label9.Size = new Size(39, 15);
+            label9.TabIndex = 8;
+            label9.Text = "TelRes";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(17, 282);
+            label10.Name = "label10";
+            label10.Size = new Size(81, 15);
+            label10.TabIndex = 9;
+            label10.Text = "FechaRegistro";
+            label10.Click += label10_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(27, 311);
+            label11.Name = "label11";
+            label11.Size = new Size(44, 15);
+            label11.TabIndex = 10;
+            label11.Text = "Estatus";
+            // 
+            // textIDEmpleado
+            // 
+            textIDEmpleado.Location = new Point(125, 12);
+            textIDEmpleado.Name = "textIDEmpleado";
+            textIDEmpleado.Size = new Size(100, 23);
+            textIDEmpleado.TabIndex = 11;
+            // 
+            // textUsuario
+            // 
+            textUsuario.Location = new Point(125, 41);
+            textUsuario.Name = "textUsuario";
+            textUsuario.Size = new Size(100, 23);
+            textUsuario.TabIndex = 12;
+            // 
+            // textNombre
+            // 
+            textNombre.Location = new Point(125, 70);
+            textNombre.Name = "textNombre";
+            textNombre.Size = new Size(100, 23);
+            textNombre.TabIndex = 13;
+            // 
+            // textApellido
+            // 
+            textApellido.Location = new Point(125, 99);
+            textApellido.Name = "textApellido";
+            textApellido.Size = new Size(100, 23);
+            textApellido.TabIndex = 14;
+            // 
+            // textTipoDocumento
+            // 
+            textTipoDocumento.Location = new Point(125, 128);
+            textTipoDocumento.Name = "textTipoDocumento";
+            textTipoDocumento.Size = new Size(100, 23);
+            textTipoDocumento.TabIndex = 15;
+            // 
+            // textDocumento
+            // 
+            textDocumento.Location = new Point(125, 157);
+            textDocumento.Name = "textDocumento";
+            textDocumento.Size = new Size(100, 23);
+            textDocumento.TabIndex = 16;
+            // 
+            // textDireccion
+            // 
+            textDireccion.Location = new Point(125, 186);
+            textDireccion.Name = "textDireccion";
+            textDireccion.Size = new Size(100, 23);
+            textDireccion.TabIndex = 17;
+            // 
+            // textTelCell
+            // 
+            textTelCell.Location = new Point(125, 215);
+            textTelCell.Name = "textTelCell";
+            textTelCell.Size = new Size(100, 23);
+            textTelCell.TabIndex = 18;
+            // 
+            // textTelRes
+            // 
+            textTelRes.Location = new Point(125, 245);
+            textTelRes.Name = "textTelRes";
+            textTelRes.Size = new Size(100, 23);
+            textTelRes.TabIndex = 19;
+            // 
+            // textFechaRegistro
+            // 
+            textFechaRegistro.Location = new Point(125, 274);
+            textFechaRegistro.Name = "textFechaRegistro";
+            textFechaRegistro.Size = new Size(100, 23);
+            textFechaRegistro.TabIndex = 20;
+            // 
+            // textEstatus
+            // 
+            textEstatus.Location = new Point(125, 303);
+            textEstatus.Name = "textEstatus";
+            textEstatus.Size = new Size(100, 23);
+            textEstatus.TabIndex = 21;
+            // 
+            // dgvEmpleado
+            // 
+            dgvEmpleado.AllowUserToAddRows = false;
+            dgvEmpleado.AllowUserToDeleteRows = false;
+            dgvEmpleado.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEmpleado.Columns.AddRange(new DataGridViewColumn[] { IDEmpleado, Usuario, Nombre, Apellido, TipoDocumento, Documento, Direccion, TellCell, TelRes, FechaRegistro, Estatus });
+            dgvEmpleado.Location = new Point(231, 20);
+            dgvEmpleado.Name = "dgvEmpleado";
+            dgvEmpleado.ReadOnly = true;
+            dgvEmpleado.RowTemplate.Height = 25;
+            dgvEmpleado.Size = new Size(580, 150);
+            dgvEmpleado.TabIndex = 22;
+            dgvEmpleado.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // IDEmpleado
+            // 
+            IDEmpleado.HeaderText = "IDEmpleado";
+            IDEmpleado.Name = "IDEmpleado";
+            IDEmpleado.ReadOnly = true;
+            // 
+            // Usuario
+            // 
+            Usuario.HeaderText = "Usuario";
+            Usuario.Name = "Usuario";
+            Usuario.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre";
+            Nombre.Name = "Nombre";
+            Nombre.ReadOnly = true;
+            // 
+            // Apellido
+            // 
+            Apellido.HeaderText = "Apellido";
+            Apellido.Name = "Apellido";
+            Apellido.ReadOnly = true;
+            // 
+            // TipoDocumento
+            // 
+            TipoDocumento.HeaderText = "TipoDocumento";
+            TipoDocumento.Name = "TipoDocumento";
+            TipoDocumento.ReadOnly = true;
+            // 
+            // Documento
+            // 
+            Documento.HeaderText = "Documento";
+            Documento.Name = "Documento";
+            Documento.ReadOnly = true;
+            // 
+            // Direccion
+            // 
+            Direccion.HeaderText = "Direccion";
+            Direccion.Name = "Direccion";
+            Direccion.ReadOnly = true;
+            // 
+            // TellCell
+            // 
+            TellCell.HeaderText = "TelCell";
+            TellCell.Name = "TellCell";
+            TellCell.ReadOnly = true;
+            // 
+            // TelRes
+            // 
+            TelRes.HeaderText = "TelRes";
+            TelRes.Name = "TelRes";
+            TelRes.ReadOnly = true;
+            // 
+            // FechaRegistro
+            // 
+            FechaRegistro.HeaderText = "FechaResgistro";
+            FechaRegistro.Name = "FechaRegistro";
+            FechaRegistro.ReadOnly = true;
+            // 
+            // Estatus
+            // 
+            Estatus.HeaderText = "Estatus";
+            Estatus.Name = "Estatus";
+            Estatus.ReadOnly = true;
+            // 
+            // frmEmpleado
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(694, 376);
+            Controls.Add(dgvEmpleado);
+            Controls.Add(textEstatus);
+            Controls.Add(textFechaRegistro);
+            Controls.Add(textTelRes);
+            Controls.Add(textTelCell);
+            Controls.Add(textDireccion);
+            Controls.Add(textDocumento);
+            Controls.Add(textTipoDocumento);
+            Controls.Add(textApellido);
+            Controls.Add(textNombre);
+            Controls.Add(textUsuario);
+            Controls.Add(textIDEmpleado);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmEmpleado";
+            Text = "frmEmpleado";
+            Load += frmEmpleado_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvEmpleado).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox textIDEmpleado;
+        private TextBox textUsuario;
+        private TextBox textNombre;
+        private TextBox textApellido;
+        private TextBox textTipoDocumento;
+        private TextBox textDocumento;
+        private TextBox textDireccion;
+        private TextBox textTelCell;
+        private TextBox textTelRes;
+        private TextBox textFechaRegistro;
+        private TextBox textEstatus;
+        private DataGridView dgvEmpleado;
+        private DataGridViewTextBoxColumn IDEmpleado;
+        private DataGridViewTextBoxColumn Usuario;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Apellido;
+        private DataGridViewTextBoxColumn TipoDocumento;
+        private DataGridViewTextBoxColumn Documento;
+        private DataGridViewTextBoxColumn Direccion;
+        private DataGridViewTextBoxColumn TellCell;
+        private DataGridViewTextBoxColumn TelRes;
+        private DataGridViewTextBoxColumn FechaRegistro;
+        private DataGridViewTextBoxColumn Estatus;
+    }
+}
---
+++ SistemaGym.UI.Windows/frmEmpleado.cs
@@ -0,0 +1,28 @@
+﻿using SistemaGym.BLL;
+using SistemaGym.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGym.UI.Windows
+{
+    public partial class frmEmpleado : Form
+    {
+        EmpleadoBLL empleadoBLL = new EmpleadoBLL();
+        public frmEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEmpleado_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
