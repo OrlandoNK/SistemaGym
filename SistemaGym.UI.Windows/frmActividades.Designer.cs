ea711fb4ef89765f100c7219b250eb65f7255678
KingHernan07 <145953703+KingHernan07@users.noreply.github.com>
2023-11-09 00:58:49 +0100

    MantenimientoActividadesHecho
    Todo Listo

---
+++ SistemaGym.UI.Windows/frmActividades.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace SistemaGym.UI.Windows
+{
+    partial class frmActividades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textIDActividad = new TextBox();
+            textIDTipoActividades = new TextBox();
+            textNombre = new TextBox();
+            textArea = new TextBox();
+            textEncargadoActividad = new TextBox();
+            textDia = new TextBox();
+            textHoraInicio = new TextBox();
+            textHoraCierre = new TextBox();
+            textEstatus = new TextBox();
+            dataGridView1 = new DataGridView();
+            IDActividad = new DataGridViewTextBoxColumn();
+            IDTipoActividades = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Area = new DataGridViewTextBoxColumn();
+            EncargadoActividades = new DataGridViewTextBoxColumn();
+            Dia = new DataGridViewTextBoxColumn();
+            HoraInicio = new DataGridViewTextBoxColumn();
+            HoraCierre = new DataGridViewTextBoxColumn();
+            Estatus = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(58, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 15);
+            label1.TabIndex = 0;
+            label1.Text = "IDActividad";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(58, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 15);
+            label2.TabIndex = 1;
+            label2.Text = "IDTipoAtividades";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(58, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(58, 188);
+            label4.Name = "label4";
+            label4.Size = new Size(31, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Area";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(58, 229);
+            label5.Name = "label5";
+            label5.Size = new Size(113, 15);
+            label5.TabIndex = 4;
+            label5.Text = "EncargadoActividad";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(58, 268);
+            label6.Name = "label6";
+            label6.RightToLeft = RightToLeft.No;
+            label6.Size = new Size(24, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Dia";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(58, 306);
+            label7.Name = "label7";
+            label7.Size = new Size(62, 15);
+            label7.TabIndex = 6;
+            label7.Text = "HoraInicio";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(58, 340);
+            label8.Name = "label8";
+            label8.Size = new Size(64, 15);
+            label8.TabIndex = 7;
+            label8.Text = "HoraCierre";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(58, 378);
+            label9.Name = "label9";
+            label9.Size = new Size(44, 15);
+            label9.TabIndex = 8;
+            label9.Text = "Estatus";
+            // 
+            // textIDActividad
+            // 
+            textIDActividad.Location = new Point(182, 71);
+            textIDActividad.Name = "textIDActividad";
+            textIDActividad.Size = new Size(100, 23);
+            textIDActividad.TabIndex = 9;
+            // 
+            // textIDTipoActividades
+            // 
+            textIDTipoActividades.Location = new Point(182, 108);
+            textIDTipoActividades.Name = "textIDTipoActividades";
+            textIDTipoActividades.Size = new Size(100, 23);
+            textIDTipoActividades.TabIndex = 10;
+            // 
+            // textNombre
+            // 
+            textNombre.Location = new Point(182, 147);
+            textNombre.Name = "textNombre";
+            textNombre.Size = new Size(100, 23);
+            textNombre.TabIndex = 11;
+            // 
+            // textArea
+            // 
+            textArea.Location = new Point(182, 188);
+            textArea.Name = "textArea";
+            textArea.Size = new Size(100, 23);
+            textArea.TabIndex = 12;
+            // 
+            // textEncargadoActividad
+            // 
+            textEncargadoActividad.Location = new Point(182, 229);
+            textEncargadoActividad.Name = "textEncargadoActividad";
+            textEncargadoActividad.Size = new Size(100, 23);
+            textEncargadoActividad.TabIndex = 13;
+            // 
+            // textDia
+            // 
+            textDia.Location = new Point(182, 268);
+            textDia.Name = "textDia";
+            textDia.Size = new Size(100, 23);
+            textDia.TabIndex = 14;
+            // 
+            // textHoraInicio
+            // 
+            textHoraInicio.Location = new Point(182, 306);
+            textHoraInicio.Name = "textHoraInicio";
+            textHoraInicio.Size = new Size(100, 23);
+            textHoraInicio.TabIndex = 15;
+            // 
+            // textHoraCierre
+            // 
+            textHoraCierre.Location = new Point(182, 340);
+            textHoraCierre.Name = "textHoraCierre";
+            textHoraCierre.Size = new Size(100, 23);
+            textHoraCierre.TabIndex = 16;
+            // 
+            // textEstatus
+            // 
+            textEstatus.Location = new Point(182, 378);
+            textEstatus.Name = "textEstatus";
+            textEstatus.Size = new Size(100, 23);
+            textEstatus.TabIndex = 17;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { IDActividad, IDTipoActividades, Nombre, Area, EncargadoActividades, Dia, HoraInicio, HoraCierre, Estatus });
+            dataGridView1.Location = new Point(301, 69);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(296, 188);
+            dataGridView1.TabIndex = 18;
+            // 
+            // IDActividad
+            // 
+            IDActividad.HeaderText = "IDAcitivdad";
+            IDActividad.Name = "IDActividad";
+            IDActividad.ReadOnly = true;
+            // 
+            // IDTipoActividades
+            // 
+            IDTipoActividades.HeaderText = "IDTipoActividades";
+            IDTipoActividades.Name = "IDTipoActividades";
+            IDTipoActividades.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre";
+            Nombre.Name = "Nombre";
+            Nombre.ReadOnly = true;
+            // 
+            // Area
+            // 
+            Area.HeaderText = "Area";
+            Area.Name = "Area";
+            Area.ReadOnly = true;
+            // 
+            // EncargadoActividades
+            // 
+            EncargadoActividades.HeaderText = "EncargadoActividades";
+            EncargadoActividades.Name = "EncargadoActividades";
+            EncargadoActividades.ReadOnly = true;
+            // 
+            // Dia
+            // 
+            Dia.HeaderText = "Dia";
+            Dia.Name = "Dia";
+            Dia.ReadOnly = true;
+            // 
+            // HoraInicio
+            // 
+            HoraInicio.HeaderText = "HoraInicio";
+            HoraInicio.Name = "HoraInicio";
+            HoraInicio.ReadOnly = true;
+            // 
+            // HoraCierre
+            // 
+            HoraCierre.HeaderText = "HoraCierre";
+            HoraCierre.Name = "HoraCierre";
+            HoraCierre.ReadOnly = true;
+            // 
+            // Estatus
+            // 
+            Estatus.HeaderText = "Estatus";
+            Estatus.Name = "Estatus";
+            Estatus.ReadOnly = true;
+            // 
+            // frmActividades
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(textEstatus);
+            Controls.Add(textHoraCierre);
+            Controls.Add(textHoraInicio);
+            Controls.Add(textDia);
+            Controls.Add(textEncargadoActividad);
+            Controls.Add(textArea);
+            Controls.Add(textNombre);
+            Controls.Add(textIDTipoActividades);
+            Controls.Add(textIDActividad);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmActividades";
+            Text = "frmActividades";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textIDActividad;
+        private TextBox textIDTipoActividades;
+        private TextBox textNombre;
+        private TextBox textArea;
+        private TextBox textEncargadoActividad;
+        private TextBox textDia;
+        private TextBox textHoraInicio;
+        private TextBox textHoraCierre;
+        private TextBox textEstatus;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn IDActividad;
+        private DataGridViewTextBoxColumn IDTipoActividades;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Area;
+        private DataGridViewTextBoxColumn EncargadoActividades;
+        private DataGridViewTextBoxColumn Dia;
+        private DataGridViewTextBoxColumn HoraInicio;
+        private DataGridViewTextBoxColumn HoraCierre;
+        private DataGridViewTextBoxColumn Estatus;
+    }
+}
