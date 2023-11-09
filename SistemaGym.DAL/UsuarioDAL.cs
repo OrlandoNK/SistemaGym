fdb14ae111bf58348a20cc096c9e04d52d05d8cb
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-11-08 22:49:32 -0400

    Arreglos Login

--- SistemaGym.DAL/UsuarioDAL.cs
+++ SistemaGym.DAL/UsuarioDAL.cs
@@ -18,7 +18,7 @@ namespace SistemaGym.DAL
 
             int count;
             Conexion.Open();
-            string Query = "SELECT COUNT(*) FROM Usuario WHERE Usuario =@nombreusuario AND Contraseña =@contrasena";
+            string Query = "SELECT COUNT(*) FROM Usuarios WHERE NombreUsuario =@nombreusuario AND Contrasena =@contrasena";
             SqlCommand cmd = new SqlCommand(Query, Conexion);
             cmd.Parameters.AddWithValue("@nombreusuario", usuario.NombreUsuario);
             cmd.Parameters.AddWithValue("@contrasena", usuario.Contrasena);
