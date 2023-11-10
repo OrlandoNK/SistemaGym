b47f4726f79ce33ccc095f8aec069606e139d07c
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-10 02:06:07 -0400

    Diseño Interfaz Login

--- SistemaGym.UI.Windows/Login.Designer.cs
+++ SistemaGym.UI.Windows/Login.Designer.cs
@@ -28,78 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnIniciarSesion = new Button();
-            label1 = new Label();
-            label2 = new Label();
-            txtUsuario = new TextBox();
-            txtContrasena = new TextBox();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            TxbUsuario = new Guna.UI2.WinForms.Guna2TextBox();
+            TxbContraseña = new Guna.UI2.WinForms.Guna2TextBox();
+            btnEntrar = new Guna.UI2.WinForms.Guna2Button();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            lnkRegistrese = new LinkLabel();
+            guna2Panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            guna2Panel3.SuspendLayout();
             SuspendLayout();
             // 
-            // btnIniciarSesion
+            // TxbUsuario
             // 
-            btnIniciarSesion.Location = new Point(339, 246);
-            btnIniciarSesion.Name = "btnIniciarSesion";
-            btnIniciarSesion.Size = new Size(122, 23);
-            btnIniciarSesion.TabIndex = 0;
-            btnIniciarSesion.Text = "Iniciar Sesion";
-            btnIniciarSesion.UseVisualStyleBackColor = true;
-            btnIniciarSesion.Click += btnIniciarSesion_Click;
+            TxbUsuario.BorderRadius = 5;
+            TxbUsuario.CustomizableEdges = customizableEdges1;
+            TxbUsuario.DefaultText = "";
+            TxbUsuario.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            TxbUsuario.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            TxbUsuario.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            TxbUsuario.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            TxbUsuario.FillColor = Color.Honeydew;
+            TxbUsuario.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            TxbUsuario.Font = new Font("Microsoft YaHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            TxbUsuario.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            TxbUsuario.Location = new Point(50, 101);
+            TxbUsuario.Name = "TxbUsuario";
+            TxbUsuario.PasswordChar = '\0';
+            TxbUsuario.PlaceholderText = "Usuario";
+            TxbUsuario.SelectedText = "";
+            TxbUsuario.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            TxbUsuario.Size = new Size(335, 41);
+            TxbUsuario.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            TxbUsuario.TabIndex = 0;
             // 
-            // label1
+            // TxbContraseña
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(218, 117);
-            label1.Name = "label1";
-            label1.Size = new Size(47, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Usuario";
+            TxbContraseña.BorderRadius = 5;
+            TxbContraseña.CustomizableEdges = customizableEdges3;
+            TxbContraseña.DefaultText = "";
+            TxbContraseña.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            TxbContraseña.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            TxbContraseña.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            TxbContraseña.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            TxbContraseña.FillColor = Color.Honeydew;
+            TxbContraseña.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            TxbContraseña.Font = new Font("Microsoft YaHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            TxbContraseña.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            TxbContraseña.Location = new Point(50, 170);
+            TxbContraseña.Name = "TxbContraseña";
+            TxbContraseña.PasswordChar = '\0';
+            TxbContraseña.PlaceholderText = "Contraseña";
+            TxbContraseña.SelectedText = "";
+            TxbContraseña.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            TxbContraseña.Size = new Size(335, 41);
+            TxbContraseña.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            TxbContraseña.TabIndex = 1;
             // 
-            // label2
+            // btnEntrar
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(218, 165);
-            label2.Name = "label2";
-            label2.Size = new Size(67, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Contraseña";
+            btnEntrar.BorderColor = Color.YellowGreen;
+            btnEntrar.BorderRadius = 5;
+            btnEntrar.BorderThickness = 1;
+            btnEntrar.CustomizableEdges = customizableEdges5;
+            btnEntrar.DisabledState.BorderColor = Color.DarkGray;
+            btnEntrar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnEntrar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnEntrar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnEntrar.FillColor = Color.Transparent;
+            btnEntrar.Font = new Font("Microsoft YaHei UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEntrar.ForeColor = Color.SeaGreen;
+            btnEntrar.Location = new Point(50, 299);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            btnEntrar.Size = new Size(335, 45);
+            btnEntrar.TabIndex = 2;
+            btnEntrar.Text = "Iniciar Sesion";
             // 
-            // txtUsuario
+            // guna2Panel1
             // 
-            txtUsuario.Location = new Point(306, 109);
-            txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(181, 23);
-            txtUsuario.TabIndex = 3;
+            guna2Panel1.BackColor = Color.FromArgb(39, 57, 80);
+            guna2Panel1.CustomizableEdges = customizableEdges7;
+            guna2Panel1.Dock = DockStyle.Top;
+            guna2Panel1.Location = new Point(0, 0);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Panel1.Size = new Size(657, 59);
+            guna2Panel1.TabIndex = 3;
             // 
-            // txtContrasena
+            // guna2Panel2
             // 
-            txtContrasena.Location = new Point(306, 157);
-            txtContrasena.Name = "txtContrasena";
-            txtContrasena.Size = new Size(181, 23);
-            txtContrasena.TabIndex = 4;
+            guna2Panel2.Controls.Add(guna2PictureBox1);
+            guna2Panel2.CustomizableEdges = customizableEdges11;
+            guna2Panel2.Dock = DockStyle.Left;
+            guna2Panel2.Location = new Point(0, 59);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2Panel2.Size = new Size(223, 444);
+            guna2Panel2.TabIndex = 4;
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.CustomizableEdges = customizableEdges9;
+            guna2PictureBox1.Dock = DockStyle.Fill;
+            guna2PictureBox1.FillColor = Color.Transparent;
+            guna2PictureBox1.Image = Properties.Resources.Comfort_GYM___Login;
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(0, 0);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2PictureBox1.Size = new Size(223, 444);
+            guna2PictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2PictureBox1.TabIndex = 0;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Panel3
+            // 
+            guna2Panel3.BackColor = Color.Honeydew;
+            guna2Panel3.Controls.Add(lnkRegistrese);
+            guna2Panel3.Controls.Add(TxbUsuario);
+            guna2Panel3.Controls.Add(btnEntrar);
+            guna2Panel3.Controls.Add(TxbContraseña);
+            guna2Panel3.CustomizableEdges = customizableEdges13;
+            guna2Panel3.Dock = DockStyle.Fill;
+            guna2Panel3.Location = new Point(223, 59);
+            guna2Panel3.Name = "guna2Panel3";
+            guna2Panel3.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            guna2Panel3.Size = new Size(434, 444);
+            guna2Panel3.TabIndex = 5;
+            // 
+            // lnkRegistrese
+            // 
+            lnkRegistrese.Cursor = Cursors.Hand;
+            lnkRegistrese.Dock = DockStyle.Bottom;
+            lnkRegistrese.Font = new Font("Microsoft YaHei UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            lnkRegistrese.LinkColor = Color.DarkSlateGray;
+            lnkRegistrese.Location = new Point(0, 397);
+            lnkRegistrese.Name = "lnkRegistrese";
+            lnkRegistrese.Size = new Size(434, 47);
+            lnkRegistrese.TabIndex = 3;
+            lnkRegistrese.TabStop = true;
+            lnkRegistrese.Text = "Registrese Aqui";
+            lnkRegistrese.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(txtContrasena);
-            Controls.Add(txtUsuario);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(btnIniciarSesion);
+            ClientSize = new Size(657, 503);
+            Controls.Add(guna2Panel3);
+            Controls.Add(guna2Panel2);
+            Controls.Add(guna2Panel1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Login";
-            Text = "Login";
-            Load += Login_Load;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Comfort GYM - Pantalla de Inicio de Sesion";
+            guna2Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            guna2Panel3.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Button btnIniciarSesion;
-        private Label label1;
-        private Label label2;
-        private TextBox txtUsuario;
-        private TextBox txtContrasena;
+        private Guna.UI2.WinForms.Guna2TextBox TxbUsuario;
+        private Guna.UI2.WinForms.Guna2TextBox TxbContraseña;
+        private Guna.UI2.WinForms.Guna2Button btnEntrar;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private LinkLabel lnkRegistrese;
     }
 }
