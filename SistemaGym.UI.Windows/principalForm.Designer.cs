520bc39e0e4503c51d751d922840932bfe849669
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2023-11-10 00:20:12 -0400

    Diseño Interfaz Pantalla Principal

---
+++ SistemaGym.UI.Windows/principalForm.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace SistemaGym.UI.Windows
+{
+    partial class principalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges27 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges28 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges29 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges30 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            btnTipoMembresia = new Guna.UI2.WinForms.Guna2Button();
+            btnMemberList = new Guna.UI2.WinForms.Guna2Button();
+            btnEmpleados = new Guna.UI2.WinForms.Guna2Button();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            chbxdesplegable = new Guna.UI2.WinForms.Guna2ImageCheckBox();
+            lblusuario = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2vSeparator1 = new Guna.UI2.WinForms.Guna2VSeparator();
+            panelDesplegable = new Panel();
+            btncerrarSesion = new Guna.UI2.WinForms.Guna2Button();
+            guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            guna2Panel2.SuspendLayout();
+            panelDesplegable.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackColor = Color.FromArgb(39, 57, 80);
+            guna2Panel1.Controls.Add(btnTipoMembresia);
+            guna2Panel1.Controls.Add(btnMemberList);
+            guna2Panel1.Controls.Add(btnEmpleados);
+            guna2Panel1.Controls.Add(guna2PictureBox1);
+            guna2Panel1.CustomizableEdges = customizableEdges24;
+            guna2Panel1.Dock = DockStyle.Left;
+            guna2Panel1.Location = new Point(0, 0);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges25;
+            guna2Panel1.Size = new Size(216, 557);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // btnTipoMembresia
+            // 
+            btnTipoMembresia.Anchor = AnchorStyles.None;
+            btnTipoMembresia.Cursor = Cursors.Hand;
+            btnTipoMembresia.CustomizableEdges = customizableEdges16;
+            btnTipoMembresia.DisabledState.BorderColor = Color.DarkGray;
+            btnTipoMembresia.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnTipoMembresia.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnTipoMembresia.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnTipoMembresia.FillColor = Color.Transparent;
+            btnTipoMembresia.Font = new Font("Microsoft YaHei UI", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTipoMembresia.ForeColor = Color.YellowGreen;
+            btnTipoMembresia.HoverState.FillColor = Color.LightGreen;
+            btnTipoMembresia.HoverState.ForeColor = Color.DarkSlateGray;
+            btnTipoMembresia.Image = Properties.Resources.tiposmembresia;
+            btnTipoMembresia.ImageAlign = HorizontalAlignment.Left;
+            btnTipoMembresia.ImageSize = new Size(25, 25);
+            btnTipoMembresia.Location = new Point(0, 313);
+            btnTipoMembresia.Name = "btnTipoMembresia";
+            btnTipoMembresia.PressedColor = Color.SeaGreen;
+            btnTipoMembresia.ShadowDecoration.CustomizableEdges = customizableEdges17;
+            btnTipoMembresia.Size = new Size(216, 45);
+            btnTipoMembresia.TabIndex = 3;
+            btnTipoMembresia.Text = "Planes Membresia";
+            btnTipoMembresia.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btnMemberList
+            // 
+            btnMemberList.Anchor = AnchorStyles.None;
+            btnMemberList.Cursor = Cursors.Hand;
+            btnMemberList.CustomizableEdges = customizableEdges18;
+            btnMemberList.DisabledState.BorderColor = Color.DarkGray;
+            btnMemberList.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnMemberList.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnMemberList.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnMemberList.FillColor = Color.Transparent;
+            btnMemberList.Font = new Font("Microsoft YaHei UI", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            btnMemberList.ForeColor = Color.YellowGreen;
+            btnMemberList.HoverState.FillColor = Color.LightGreen;
+            btnMemberList.HoverState.ForeColor = Color.DarkSlateGray;
+            btnMemberList.Image = Properties.Resources.listaMiembros1;
+            btnMemberList.ImageAlign = HorizontalAlignment.Left;
+            btnMemberList.ImageSize = new Size(25, 25);
+            btnMemberList.Location = new Point(0, 248);
+            btnMemberList.Name = "btnMemberList";
+            btnMemberList.PressedColor = Color.SeaGreen;
+            btnMemberList.ShadowDecoration.CustomizableEdges = customizableEdges19;
+            btnMemberList.Size = new Size(216, 45);
+            btnMemberList.TabIndex = 2;
+            btnMemberList.Text = "Lista de Miembros";
+            btnMemberList.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btnEmpleados
+            // 
+            btnEmpleados.Anchor = AnchorStyles.None;
+            btnEmpleados.Cursor = Cursors.Hand;
+            btnEmpleados.CustomizableEdges = customizableEdges20;
+            btnEmpleados.DisabledState.BorderColor = Color.DarkGray;
+            btnEmpleados.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnEmpleados.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnEmpleados.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnEmpleados.FillColor = Color.Transparent;
+            btnEmpleados.Font = new Font("Microsoft YaHei UI", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEmpleados.ForeColor = Color.YellowGreen;
+            btnEmpleados.HoverState.FillColor = Color.LightGreen;
+            btnEmpleados.HoverState.ForeColor = Color.DarkSlateGray;
+            btnEmpleados.Image = Properties.Resources.empleado;
+            btnEmpleados.ImageAlign = HorizontalAlignment.Left;
+            btnEmpleados.ImageSize = new Size(25, 25);
+            btnEmpleados.Location = new Point(0, 183);
+            btnEmpleados.Name = "btnEmpleados";
+            btnEmpleados.PressedColor = Color.SeaGreen;
+            btnEmpleados.ShadowDecoration.CustomizableEdges = customizableEdges21;
+            btnEmpleados.Size = new Size(216, 45);
+            btnEmpleados.TabIndex = 1;
+            btnEmpleados.Text = "Empleados";
+            btnEmpleados.TextAlign = HorizontalAlignment.Right;
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.CustomizableEdges = customizableEdges22;
+            guna2PictureBox1.Dock = DockStyle.Top;
+            guna2PictureBox1.FillColor = Color.Transparent;
+            guna2PictureBox1.Image = Properties.Resources.COMFORT_GYM;
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(0, 0);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges23;
+            guna2PictureBox1.Size = new Size(216, 145);
+            guna2PictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2PictureBox1.TabIndex = 0;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.BackColor = Color.White;
+            guna2Panel2.Controls.Add(chbxdesplegable);
+            guna2Panel2.Controls.Add(lblusuario);
+            guna2Panel2.Controls.Add(guna2vSeparator1);
+            guna2Panel2.CustomizableEdges = customizableEdges27;
+            guna2Panel2.Dock = DockStyle.Top;
+            guna2Panel2.Location = new Point(216, 0);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges28;
+            guna2Panel2.Size = new Size(769, 56);
+            guna2Panel2.TabIndex = 1;
+            // 
+            // chbxdesplegable
+            // 
+            chbxdesplegable.CheckedState.Image = Properties.Resources.desplegable_false;
+            chbxdesplegable.Image = Properties.Resources.desplegable_true_2;
+            chbxdesplegable.ImageOffset = new Point(0, 0);
+            chbxdesplegable.ImageRotate = 0F;
+            chbxdesplegable.Location = new Point(727, 15);
+            chbxdesplegable.Name = "chbxdesplegable";
+            chbxdesplegable.ShadowDecoration.CustomizableEdges = customizableEdges26;
+            chbxdesplegable.Size = new Size(33, 27);
+            chbxdesplegable.TabIndex = 2;
+            chbxdesplegable.CheckedChanged += chbxdesplegable_CheckedChanged;
+            // 
+            // lblusuario
+            // 
+            lblusuario.Anchor = AnchorStyles.None;
+            lblusuario.AutoSize = false;
+            lblusuario.BackColor = Color.Transparent;
+            lblusuario.Font = new Font("Microsoft YaHei UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            lblusuario.Location = new Point(529, 15);
+            lblusuario.Name = "lblusuario";
+            lblusuario.Size = new Size(192, 27);
+            lblusuario.TabIndex = 1;
+            lblusuario.Text = "usuario";
+            lblusuario.TextAlignment = ContentAlignment.MiddleCenter;
+            // 
+            // guna2vSeparator1
+            // 
+            guna2vSeparator1.FillThickness = 2;
+            guna2vSeparator1.Location = new Point(512, 2);
+            guna2vSeparator1.Name = "guna2vSeparator1";
+            guna2vSeparator1.Size = new Size(24, 53);
+            guna2vSeparator1.TabIndex = 0;
+            // 
+            // panelDesplegable
+            // 
+            panelDesplegable.BackColor = Color.FromArgb(39, 57, 80);
+            panelDesplegable.Controls.Add(btncerrarSesion);
+            panelDesplegable.Location = new Point(745, 58);
+            panelDesplegable.Name = "panelDesplegable";
+            panelDesplegable.Size = new Size(236, 83);
+            panelDesplegable.TabIndex = 3;
+            panelDesplegable.Visible = false;
+            // 
+            // btncerrarSesion
+            // 
+            btncerrarSesion.Anchor = AnchorStyles.None;
+            btncerrarSesion.Cursor = Cursors.Hand;
+            btncerrarSesion.CustomizableEdges = customizableEdges29;
+            btncerrarSesion.DisabledState.BorderColor = Color.DarkGray;
+            btncerrarSesion.DisabledState.CustomBorderColor = Color.DarkGray;
+            btncerrarSesion.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btncerrarSesion.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btncerrarSesion.FillColor = Color.Transparent;
+            btncerrarSesion.Font = new Font("Microsoft YaHei UI", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            btncerrarSesion.ForeColor = Color.MintCream;
+            btncerrarSesion.HoverState.FillColor = Color.LightGreen;
+            btncerrarSesion.HoverState.ForeColor = Color.DarkSlateGray;
+            btncerrarSesion.Image = Properties.Resources.cerrar_sesion;
+            btncerrarSesion.ImageAlign = HorizontalAlignment.Left;
+            btncerrarSesion.ImageSize = new Size(25, 25);
+            btncerrarSesion.Location = new Point(3, 9);
+            btncerrarSesion.Name = "btncerrarSesion";
+            btncerrarSesion.PressedColor = Color.SeaGreen;
+            btncerrarSesion.ShadowDecoration.CustomizableEdges = customizableEdges30;
+            btncerrarSesion.Size = new Size(230, 40);
+            btncerrarSesion.TabIndex = 4;
+            btncerrarSesion.Text = "Cerrar Sesion";
+            btncerrarSesion.TextAlign = HorizontalAlignment.Right;
+            // 
+            // principalForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(985, 557);
+            Controls.Add(panelDesplegable);
+            Controls.Add(guna2Panel2);
+            Controls.Add(guna2Panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            IsMdiContainer = true;
+            Name = "principalForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Comfort GYM - Pantalla Principal";
+            WindowState = FormWindowState.Maximized;
+            guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            guna2Panel2.ResumeLayout(false);
+            panelDesplegable.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnEmpleados;
+        private Guna.UI2.WinForms.Guna2Button btnMemberList;
+        private Guna.UI2.WinForms.Guna2Button btnTipoMembresia;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2VSeparator guna2vSeparator1;
+        private Guna.UI2.WinForms.Guna2ImageCheckBox chbxdesplegable;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblusuario;
+        private Panel panelDesplegable;
+        private Guna.UI2.WinForms.Guna2Button btncerrarSesion;
+    }
+}
