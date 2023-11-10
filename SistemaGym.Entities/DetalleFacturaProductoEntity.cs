85d5f5b4a156342d9388836755273851a274735f
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-11-10 00:55:48 -0400

    Cambios

--- SistemaGym.Entities/DetalleFacturaProductoEntity.cs
+++ SistemaGym.Entities/DetalleFacturaProductoEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,14 @@ namespace SistemaGym.Entities
 {
     public class DetalleFacturaProductoEntity
     {
-        public int ID {  get; set; }
+        public int IDDetallesFacturaProducto {  get; set; }
         public int IDFacturaProducto {  get; set; }
         public int IDProducto {  get; set; }
-        public int Cantidad {  get; set; }
-        public decimal SubTotal {  get; set; }
-        public decimal Impuesto {  get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public decimal SubTotal{ get{ return Cantidad * Precio; } set { } }
+        public decimal Descuento { get; set; }
+        public decimal Itbis { get { return SubTotal * (18 / 100); } set { } }
+        public decimal Total { get {  return SubTotal - Descuento + Itbis; } set { } }
     }
 }
