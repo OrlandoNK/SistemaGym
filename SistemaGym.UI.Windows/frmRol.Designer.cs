071383adb13a60bc0a9de0f0b949a7047c1e5c2b
KingHernan07 <145953703+KingHernan07@users.noreply.github.com>
2023-11-28 05:04:08 +0100

    MantenimientoRolHecho
    Hecho

--- SistemaGym.UI.Windows/frmRol.Designer.cs
+++ SistemaGym.UI.Windows/frmRol.Designer.cs
@@ -28,14 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            textIDRol = new TextBox();
-            textNombre = new TextBox();
-            textDescripcion = new TextBox();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            txtIDRol = new TextBox();
+            txtNombre = new TextBox();
+            txtDescripcion = new TextBox();
+            dgvRol = new DataGridView();
+            IDRol = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Descripcion = new DataGridViewTextBoxColumn();
+            btnNuevo = new Label();
+            btnGuardar = new Label();
+            btnCancelar = new Label();
+            label7 = new Label();
+            txtID = new TextBox();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)dgvRol).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -66,54 +77,130 @@
             label3.TabIndex = 2;
             label3.Text = "Descripcion";
             // 
-            // textIDRol
+            // txtIDRol
             // 
-            textIDRol.Location = new Point(107, 53);
-            textIDRol.Name = "textIDRol";
-            textIDRol.Size = new Size(100, 23);
-            textIDRol.TabIndex = 3;
+            txtIDRol.Location = new Point(107, 53);
+            txtIDRol.Name = "txtIDRol";
+            txtIDRol.Size = new Size(100, 23);
+            txtIDRol.TabIndex = 3;
             // 
-            // textNombre
+            // txtNombre
             // 
-            textNombre.Location = new Point(107, 86);
-            textNombre.Name = "textNombre";
-            textNombre.Size = new Size(100, 23);
-            textNombre.TabIndex = 4;
+            txtNombre.Location = new Point(107, 86);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 4;
             // 
-            // textDescripcion
+            // txtDescripcion
             // 
-            textDescripcion.Location = new Point(107, 120);
-            textDescripcion.Name = "textDescripcion";
-            textDescripcion.Size = new Size(100, 23);
-            textDescripcion.TabIndex = 5;
+            txtDescripcion.Location = new Point(107, 120);
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(100, 23);
+            txtDescripcion.TabIndex = 5;
             // 
-            // dataGridView1
+            // dgvRol
             // 
-            dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.AllowUserToDeleteRows = false;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(263, 27);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.ReadOnly = true;
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(240, 150);
-            dataGridView1.TabIndex = 6;
+            dgvRol.AllowUserToAddRows = false;
+            dgvRol.AllowUserToDeleteRows = false;
+            dgvRol.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRol.Columns.AddRange(new DataGridViewColumn[] { IDRol, Nombre, Descripcion });
+            dgvRol.Location = new Point(240, 27);
+            dgvRol.Name = "dgvRol";
+            dgvRol.ReadOnly = true;
+            dgvRol.RowTemplate.Height = 25;
+            dgvRol.Size = new Size(346, 150);
+            dgvRol.TabIndex = 6;
+            dgvRol.CellContentClick += dgvRol_CellContentClick;
+            // 
+            // IDRol
+            // 
+            IDRol.HeaderText = "IDRol";
+            IDRol.Name = "IDRol";
+            IDRol.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre";
+            Nombre.Name = "Nombre";
+            Nombre.ReadOnly = true;
+            // 
+            // Descripcion
+            // 
+            Descripcion.HeaderText = "Descripcion";
+            Descripcion.Name = "Descripcion";
+            Descripcion.ReadOnly = true;
+            // 
+            // btnNuevo
+            // 
+            btnNuevo.AutoSize = true;
+            btnNuevo.Location = new Point(52, 360);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(42, 15);
+            btnNuevo.TabIndex = 7;
+            btnNuevo.Text = "Nuevo";
+            btnNuevo.Click += btnNuevo_Click;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.AutoSize = true;
+            btnGuardar.Location = new Point(159, 360);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(49, 15);
+            btnGuardar.TabIndex = 8;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.AutoSize = true;
+            btnCancelar.Location = new Point(284, 360);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(53, 15);
+            btnCancelar.TabIndex = 9;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(409, 360);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 10;
+            label7.Text = "label7";
+            // 
+            // txtID
+            // 
+            txtID.Location = new Point(107, 272);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(100, 23);
+            txtID.TabIndex = 11;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
             // 
             // frmRol
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(559, 450);
-            Controls.Add(dataGridView1);
-            Controls.Add(textDescripcion);
-            Controls.Add(textNombre);
-            Controls.Add(textIDRol);
+            ClientSize = new Size(621, 450);
+            Controls.Add(txtID);
+            Controls.Add(label7);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnGuardar);
+            Controls.Add(btnNuevo);
+            Controls.Add(dgvRol);
+            Controls.Add(txtDescripcion);
+            Controls.Add(txtNombre);
+            Controls.Add(txtIDRol);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "frmRol";
             Text = "frmRol";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvRol).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -123,9 +210,18 @@
         private Label label1;
         private Label label2;
         private Label label3;
-        private TextBox textIDRol;
-        private TextBox textNombre;
-        private TextBox textDescripcion;
-        private DataGridView dataGridView1;
+        private TextBox txtIDRol;
+        private TextBox txtNombre;
+        private TextBox txtDescripcion;
+        private DataGridView dgvRol;
+        private DataGridViewTextBoxColumn IDRol;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Descripcion;
+        private Label btnNuevo;
+        private Label btnGuardar;
+        private Label btnCancelar;
+        private Label label7;
+        private TextBox txtID;
+        private ErrorProvider errorProvider1;
     }
 }
--- SistemaGym.UI.Windows/frmRol.cs
+++ SistemaGym.UI.Windows/frmRol.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaGym.BLL;
+using SistemaGym.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,8 @@ namespace SistemaGym.UI.Windows
 {
     public partial class frmRol : Form
     {
+        const string sistema = "Sistema Gym";
+
         public frmRol()
         {
             InitializeComponent();
@@ -21,5 +25,79 @@ namespace SistemaGym.UI.Windows
         {
 
         }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            InicializarControles();
+        }
+        private void InicializarControles()
+        {
+            txtID.Text = "0";
+            txtIDRol.Clear();
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+            dgvRol.AutoGenerateColumns = false;
+            dgvRol.DataSource = RolBLL.MostrarRol();
+            txtNombre.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //validacion de datos
+            if (!ValidarDatos())
+            {
+                return;
+            }
+            //pasar datos de controles a un objeto
+            RolEntity Rol = new RolEntity();
+            Rol.IDRol = int.Parse(txtID.Text);
+            Rol.Nombre = txtNombre.Text;
+            Rol.Descripcion = txtDescripcion.Text;
+
+            //Mandar a guardar el objeto creado a la base de datos
+            try
+            {
+                RolBLL.Guardar(Rol);
+                MessageBox.Show("Rol guardada.", sistema, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                InicializarControles();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, sistema, MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+
+        }
+        private bool ValidarDatos()
+        {
+            bool resultado = true;
+            //inicializando los mensajes de validaciones
+            errorProvider1.Clear();
+            //verificar que en los campos obligatorios hayan datos
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                errorProvider1.SetError(txtNombre, "El nombre es obligatorio");
+                resultado = false;
+            }
+            return resultado;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void dgvRol_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex == -1)
+            {
+                return;
+
+            }
+
+            DataGridViewRow row = dgvRol.CurrentRow;
+            txtID.Text = row.Cells["IDRol"].Value?.ToString();
+            txtNombre.Text = row.Cells["Nombre"].Value?.ToString();
+            txtDescripcion.Text = row.Cells["Descripcion"].Value?.ToString();
+
+        }
     }
+
 }
+
