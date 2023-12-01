4a0b5b8e1a676e7f0fcff5308af29b92298276c9
rosaCarrasco <146145669+rosaCarrasco@users.noreply.github.com>
2023-11-30 23:33:07 -0400

    MantenimientoTipoListaCliente
    Listo

---
+++ SistemaGym.UI.Windows/frmTipoListaCliente.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace SistemaGym.UI.Windows
+{
+    partial class frmTipoListaCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtID = new TextBox();
+            txtNombre = new TextBox();
+            txtDescripcion = new TextBox();
+            btnGuardar = new Button();
+            btnNuevo = new Button();
+            btnCancelar = new Button();
+            dgvTipoListaCliente = new DataGridView();
+            ID = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Descripcion = new DataGridViewTextBoxColumn();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)dgvTipoListaCliente).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(21, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ID ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(64, 142);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Descripcion";
+            // 
+            // txtID
+            // 
+            txtID.Location = new Point(151, 41);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(100, 23);
+            txtID.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(151, 85);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 4;
+            // 
+            // txtDescripcion
+            // 
+            txtDescripcion.Location = new Point(151, 134);
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(100, 23);
+            txtDescripcion.TabIndex = 5;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(140, 232);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(75, 23);
+            btnGuardar.TabIndex = 6;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // btnNuevo
+            // 
+            btnNuevo.Location = new Point(27, 232);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(75, 23);
+            btnNuevo.TabIndex = 7;
+            btnNuevo.Text = "Nuevo";
+            btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(253, 232);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.TabIndex = 8;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // dgvTipoListaCliente
+            // 
+            dgvTipoListaCliente.AllowUserToAddRows = false;
+            dgvTipoListaCliente.AllowUserToDeleteRows = false;
+            dgvTipoListaCliente.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvTipoListaCliente.Columns.AddRange(new DataGridViewColumn[] { ID, Nombre, Descripcion });
+            dgvTipoListaCliente.Location = new Point(356, 27);
+            dgvTipoListaCliente.Name = "dgvTipoListaCliente";
+            dgvTipoListaCliente.ReadOnly = true;
+            dgvTipoListaCliente.RowTemplate.Height = 25;
+            dgvTipoListaCliente.Size = new Size(345, 155);
+            dgvTipoListaCliente.TabIndex = 9;
+            // 
+            // ID
+            // 
+            ID.HeaderText = "ID";
+            ID.Name = "ID";
+            ID.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre";
+            Nombre.Name = "Nombre";
+            Nombre.ReadOnly = true;
+            // 
+            // Descripcion
+            // 
+            Descripcion.HeaderText = "Descripcion";
+            Descripcion.Name = "Descripcion";
+            Descripcion.ReadOnly = true;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // frmTipoListaCliente
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvTipoListaCliente);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnNuevo);
+            Controls.Add(btnGuardar);
+            Controls.Add(txtDescripcion);
+            Controls.Add(txtNombre);
+            Controls.Add(txtID);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmTipoListaCliente";
+            Text = "frmTipoListaCliente";
+            ((System.ComponentModel.ISupportInitialize)dgvTipoListaCliente).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtID;
+        private TextBox txtNombre;
+        private TextBox txtDescripcion;
+        private Button btnGuardar;
+        private Button btnNuevo;
+        private Button btnCancelar;
+        private DataGridView dgvTipoListaCliente;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Descripcion;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ SistemaGym.UI.Windows/frmTipoListaCliente.cs
@@ -0,0 +1,82 @@
+﻿using SistemaGym.BLL;
+using SistemaGym.DAL;
+using SistemaGym.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGym.UI.Windows
+{
+    public partial class frmTipoListaCliente : Form
+    {
+        private string sistema = "Sistema Gym";
+        public frmTipoListaCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            InicializarControles();
+        }
+        private void InicializarControles()
+        {
+            txtID.Text = "0";
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+            dgvTipoListaCliente.AutoGenerateColumns = false;
+            dgvTipoListaCliente.DataSource = AreaEmpresaBLL.Mostrar();
+            txtNombre.Focus();
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //Validacion de datos
+            if (!ValidarDatos())
+            {
+                return;
+            }
+            //Pasar datos al objeto desde los controles
+            TipoListaClienteEntity tipoListaCliente = new TipoListaClienteEntity();
+            tipoListaCliente.IDTipoListaCliente = int.Parse(txtID.Text);
+            tipoListaCliente.Nombre = txtNombre.Text;
+            tipoListaCliente.Descripcion = txtDescripcion.Text;
+
+            try
+            {
+                TipoClienteBLL.Guardar(tipoListaCliente);
+                MessageBox.Show("Tipo Lista Cliente se guardo.", sistema, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                InicializarControles();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, sistema, MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+        private bool ValidarDatos()
+        {
+            bool resultado = true;
+            //Inicializar los mensajes
+            errorProvider1.Clear();
+            //Asegurarse que en los capos obligatoios hayan datos
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                errorProvider1.SetError(txtNombre, "Campo obligatorio");
+                resultado = false;
+            }
+            if (string.IsNullOrEmpty(txtDescripcion.Text))
+            {
+                errorProvider1.SetError(txtDescripcion, "Campo obligatorio");
+                resultado = false;
+            }
+
+            return resultado;
+        }
+
+    }
+}
+
