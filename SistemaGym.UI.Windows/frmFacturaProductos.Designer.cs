c4bd3d667b9500df4ceb59408003ecd7aafa6ed7
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-12-06 23:55:01 -0400

    en proceso factura producto

---
+++ SistemaGym.UI.Windows/frmFacturaProductos.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace SistemaGym.UI.Windows
+{
+    partial class frmFacturaProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            groupBox1 = new GroupBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            groupBox2 = new GroupBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ID Factura";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(170, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Fecha Emision";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(170, 54);
+            label5.Name = "label5";
+            label5.Size = new Size(107, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Fecha Vencimiento";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 54);
+            label6.Name = "label6";
+            label6.Size = new Size(44, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Cliente";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(283, 13);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(130, 23);
+            dateTimePicker1.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(283, 54);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(130, 23);
+            dateTimePicker2.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Location = new Point(81, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(380, 86);
+            groupBox1.TabIndex = 8;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Datos Cliente";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(231, 51);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 20);
+            textBox6.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(231, 13);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 21);
+            textBox5.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(49, 48);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 21);
+            textBox4.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(70, 16);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(79, 20);
+            textBox3.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(155, 54);
+            label8.Name = "label8";
+            label8.Size = new Size(70, 15);
+            label8.TabIndex = 8;
+            label8.Text = "Documento";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(6, 19);
+            label7.Name = "label7";
+            label7.Size = new Size(58, 15);
+            label7.TabIndex = 7;
+            label7.Text = "ID Cliente";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(155, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Tipo Cliente";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 54);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Usuario";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(dateTimePicker2);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(dateTimePicker1);
+            groupBox2.Location = new Point(534, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(440, 86);
+            groupBox2.TabIndex = 11;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Datos Factura";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(64, 16);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(89, 20);
+            textBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(64, 46);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(22, 224);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(856, 253);
+            dataGridView1.TabIndex = 12;
+            // 
+            // frmFacturaProductos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1146, 626);
+            Controls.Add(dataGridView1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "frmFacturaProductos";
+            Text = "Factura Productos";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private GroupBox groupBox1;
+        private Label label3;
+        private GroupBox groupBox2;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label8;
+        private Label label7;
+        private DataGridView dataGridView1;
+    }
+}
