4607e522df41ae9e691171542c8c9e18744d8c9d
OrlandoNK <joseorlandoabreucorniel@gmail.com>
2023-12-07 22:09:17 -0400

    fase de finalizacion mantenimiento

--- SistemaGym.UI.Windows/frmFacturaProductos.Designer.cs
+++ SistemaGym.UI.Windows/frmFacturaProductos.Designer.cs
@@ -48,9 +48,29 @@
             textBox1 = new TextBox();
             dataGridView1 = new DataGridView();
             groupBox3 = new GroupBox();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            label13 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -113,16 +133,16 @@
             groupBox1.Controls.Add(label7);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label6);
-            groupBox1.Location = new Point(81, 12);
+            groupBox1.Location = new Point(45, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(380, 86);
+            groupBox1.Size = new Size(416, 86);
             groupBox1.TabIndex = 8;
             groupBox1.TabStop = false;
             groupBox1.Text = "Datos Cliente";
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(231, 51);
+            textBox6.Location = new Point(266, 54);
             textBox6.Multiline = true;
             textBox6.Name = "textBox6";
             textBox6.Size = new Size(100, 20);
@@ -130,7 +150,7 @@
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(231, 13);
+            textBox5.Location = new Point(266, 16);
             textBox5.Multiline = true;
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(100, 21);
@@ -155,7 +175,7 @@
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(155, 54);
+            label8.Location = new Point(190, 57);
             label8.Name = "label8";
             label8.Size = new Size(70, 15);
             label8.TabIndex = 8;
@@ -173,7 +193,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(155, 19);
+            label2.Location = new Point(190, 22);
             label2.Name = "label2";
             label2.Size = new Size(70, 15);
             label2.TabIndex = 6;
@@ -223,27 +243,227 @@
             // 
             // dataGridView1
             // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(22, 224);
+            dataGridView1.Location = new Point(45, 213);
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(856, 253);
+            dataGridView1.Size = new Size(929, 267);
             dataGridView1.TabIndex = 12;
             // 
             // groupBox3
             // 
-            groupBox3.Location = new Point(81, 104);
+            groupBox3.Controls.Add(button2);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Controls.Add(label13);
+            groupBox3.Controls.Add(textBox11);
+            groupBox3.Controls.Add(textBox10);
+            groupBox3.Controls.Add(textBox9);
+            groupBox3.Controls.Add(textBox8);
+            groupBox3.Controls.Add(textBox7);
+            groupBox3.Controls.Add(label12);
+            groupBox3.Controls.Add(label11);
+            groupBox3.Controls.Add(label10);
+            groupBox3.Controls.Add(label9);
+            groupBox3.Location = new Point(45, 104);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(374, 100);
+            groupBox3.Size = new Size(929, 103);
             groupBox3.TabIndex = 13;
             groupBox3.TabStop = false;
-            groupBox3.Text = "groupBox3";
+            groupBox3.Text = "Detalles Factura";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Location = new Point(20, 39);
+            label9.Name = "label9";
+            label9.Size = new Size(57, 15);
+            label9.TabIndex = 0;
+            label9.Text = "ID Detalle";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Location = new Point(171, 39);
+            label10.Name = "label10";
+            label10.Size = new Size(56, 15);
+            label10.TabIndex = 1;
+            label10.Text = "Producto";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Location = new Point(350, 40);
+            label11.Name = "label11";
+            label11.Size = new Size(40, 15);
+            label11.TabIndex = 2;
+            label11.Text = "Precio";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Transparent;
+            label12.Location = new Point(457, 42);
+            label12.Name = "label12";
+            label12.Size = new Size(55, 15);
+            label12.TabIndex = 3;
+            label12.Text = "Cantidad";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(6, 62);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.ReadOnly = true;
+            textBox7.Size = new Size(90, 17);
+            textBox7.TabIndex = 4;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(115, 62);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.ReadOnly = true;
+            textBox8.Size = new Size(180, 17);
+            textBox8.TabIndex = 5;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(326, 62);
+            textBox9.Multiline = true;
+            textBox9.Name = "textBox9";
+            textBox9.ReadOnly = true;
+            textBox9.Size = new Size(99, 17);
+            textBox9.TabIndex = 6;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(457, 62);
+            textBox10.Multiline = true;
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(62, 17);
+            textBox10.TabIndex = 7;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(553, 62);
+            textBox11.Multiline = true;
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(62, 17);
+            textBox11.TabIndex = 8;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.Transparent;
+            label13.Location = new Point(552, 44);
+            label13.Name = "label13";
+            label13.Size = new Size(63, 15);
+            label13.TabIndex = 9;
+            label13.Text = "Descuento";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(683, 19);
+            button1.Name = "button1";
+            button1.Size = new Size(59, 36);
+            button1.TabIndex = 10;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(683, 61);
+            button2.Name = "button2";
+            button2.Size = new Size(61, 34);
+            button2.TabIndex = 11;
+            button2.Text = "Agregar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(47, 538);
+            button3.Name = "button3";
+            button3.Size = new Size(119, 49);
+            button3.TabIndex = 14;
+            button3.Text = "Nuevo";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(186, 538);
+            button4.Name = "button4";
+            button4.Size = new Size(119, 49);
+            button4.TabIndex = 15;
+            button4.Text = "Guardar";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(326, 538);
+            button5.Name = "button5";
+            button5.Size = new Size(119, 49);
+            button5.TabIndex = 16;
+            button5.Text = "Cancelar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.BackColor = Color.Transparent;
+            label14.Location = new Point(565, 517);
+            label14.Name = "label14";
+            label14.Size = new Size(51, 15);
+            label14.TabIndex = 17;
+            label14.Text = "Subtotal";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.BackColor = Color.Transparent;
+            label15.Location = new Point(775, 517);
+            label15.Name = "label15";
+            label15.Size = new Size(63, 15);
+            label15.TabIndex = 18;
+            label15.Text = "Descuento";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.BackColor = Color.Transparent;
+            label16.Location = new Point(670, 517);
+            label16.Name = "label16";
+            label16.Size = new Size(57, 15);
+            label16.TabIndex = 19;
+            label16.Text = "Impuesto";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.BackColor = Color.Transparent;
+            label17.Location = new Point(887, 517);
+            label17.Name = "label17";
+            label17.Size = new Size(32, 15);
+            label17.TabIndex = 20;
+            label17.Text = "Total";
             // 
             // frmFacturaProductos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1146, 626);
+            ClientSize = new Size(1048, 626);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
             Controls.Add(groupBox3);
             Controls.Add(dataGridView1);
             Controls.Add(groupBox2);
@@ -255,7 +475,10 @@
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -280,5 +503,24 @@
         private Label label7;
         private DataGridView dataGridView1;
         private GroupBox groupBox3;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private Label label13;
+        private TextBox textBox11;
+        private TextBox textBox10;
+        private TextBox textBox9;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
     }
 }
