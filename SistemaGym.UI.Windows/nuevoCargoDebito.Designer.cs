d1ea8af481ea9c0875644e714f538e48ec2a84bb
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2024-03-28 11:17:26 -0400

    modificado interfaz factura membresia

--- SistemaGym.UI.Windows/nuevoCargoDebito.Designer.cs
+++ SistemaGym.UI.Windows/nuevoCargoDebito.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "nuevoCargoDebito";
+            SuspendLayout();
+            // 
+            // nuevoCargoDebito
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(603, 466);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "nuevoCargoDebito";
+            StartPosition = FormStartPosition.CenterScreen;
+            ResumeLayout(false);
         }
 
         #endregion
