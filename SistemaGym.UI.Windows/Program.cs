f89f7c786011f2e18e772f736002af1e596e6bd2
Wil MPnc <145171046+Will-MPnc@users.noreply.github.com>
2024-04-05 11:23:01 -0400

    Update Program.cs

--- SistemaGym.UI.Windows/Program.cs
+++ SistemaGym.UI.Windows/Program.cs
@@ -13,7 +13,7 @@ namespace SistemaGym.UI.Windows
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmUsuario());
+            Application.Run(new LoginForm());
         }
     }
 }   
